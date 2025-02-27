be6bb6c21ef68f8fa501f81abf970b4929e93148
Abdulrahman.Ali19 <abdulrahman.ali19@outlook.com>
2025-02-27 17:55:18 +0200

    add fonts and fix theme

--- app/src/main/java/abdulrahman/ali19/aroundegypt/presentation/theme/Type.kt
+++ app/src/main/java/abdulrahman/ali19/aroundegypt/presentation/theme/Type.kt
@@ -1,34 +1,96 @@
 package abdulrahman.ali19.aroundegypt.presentation.theme
 
+import abdulrahman.ali19.aroundegypt.R
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
+
+private val Gotham = FontFamily(
+    Font(R.font.gotham_regular, FontWeight.Normal),
+    Font(R.font.gotham_bold, FontWeight.Bold),
+    Font(R.font.gotham_medium, FontWeight.Medium),
+    Font(R.font.gotham_light, FontWeight.Light),
+    Font(R.font.gotham_black, FontWeight.Black)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    displayLarge = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Black,
+        fontSize = 57.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Bold,
+        fontSize = 45.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Medium,
+        fontSize = 36.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Medium,
+        fontSize = 28.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Gotham,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = Gotham,
+        fontWeight = FontWeight.Light,
+        fontSize = 11.sp
     )
-    */
 )
