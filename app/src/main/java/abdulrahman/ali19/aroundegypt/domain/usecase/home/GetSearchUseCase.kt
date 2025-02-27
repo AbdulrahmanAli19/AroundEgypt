ee4b9ef1840ea9b9338c704ec71b9e1cbdcf28b1
Abdulrahman.Ali19 <abdulrahman.ali19@outlook.com>
2025-02-27 17:14:39 +0200

    add debounce to GetSearchUseCase

--- app/src/main/java/abdulrahman/ali19/aroundegypt/domain/usecase/home/GetSearchUseCase.kt
+++ app/src/main/java/abdulrahman/ali19/aroundegypt/domain/usecase/home/GetSearchUseCase.kt
@@ -1,9 +1,16 @@
 package abdulrahman.ali19.aroundegypt.domain.usecase.home
 
 import abdulrahman.ali19.aroundegypt.domain.repository.home.HomeRepository
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 
 class GetSearchUseCase(
     private val homeRepository: HomeRepository
 ) {
-    suspend operator fun invoke(title: String) = homeRepository.getSearch(title)
+    @OptIn(FlowPreview::class)
+    operator fun invoke(title: String) = flow {
+        emitAll(homeRepository.getSearch(title))
+    }.debounce(300L)
 }
